29cd36453ae86f3570bad2c27606725d932c6f61
LEONARDO HENRIQUE SOUZA FABRO <leonardo.hsfabro@senacsp.edu.br>
2019-12-09 19:11:44 -0300

    att 20191209

--- src/app/home/home-produtos/home-produtos.page.ts
+++ src/app/home/home-produtos/home-produtos.page.ts
@@ -2,6 +2,8 @@ import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeService } from '../shared/home.service';
+import { CarrinhoService } from 'src/app/pedidos/shared/carrinho.service';
+import { ProdutosService } from 'src/app/produtos/shared/produtos.service';
 
 @Component({
   selector: 'app-home-produtos',
@@ -10,12 +12,20 @@ import { HomeService } from '../shared/home.service';
 })
 export class HomeProdutosPage implements OnInit {
   produtos: Observable<any[]>;
+  carrinhoPossuiItens: boolean = false;
+  categorias: Observable<any[]>;
 
   constructor(private router: Router,
-              private sharedService: HomeService) { }
+              private sharedService: HomeService,
+              private produtosService: ProdutosService,
+              private carrinhoService: CarrinhoService) { }
 
   ngOnInit() {
     this.produtos = this.sharedService.getAll();
+    this.categorias = this.produtosService.getCategoriasAll();
+    this.carrinhoService.carrinhoPossuiItens().subscribe((existemItens: boolean) => {
+      this.carrinhoPossuiItens = existemItens;
+    });
   }
 
   adicionarProduto(produtoKey: string) {
